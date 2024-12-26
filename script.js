e0132fe3ce768ebaf517f2b9821d37185a40a9b9
Bhaskar Pal <bhaskar@itobuz.com>
2024-12-26 18:10:10 +0530

    work done

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
- const button=document.getElementsByClassName("btn")[0];
+const button = document.getElementsByClassName("btn")[0];
+
 function changer() {
     const color = Math.floor(Math.random() * 1000000);
     const col = "#" + color;
     document.body.style.backgroundColor = col;
-    button.textContent=col;
+    button.textContent = col;
 }
